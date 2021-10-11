0c686caa5ef101f30fb5c662f9fdcbeb7f89b7a5
Eric Cheng <68196762+import-brain@users.noreply.github.com>
2021-10-11 13:54:59 -0400

    feat: get latest products API

--- blindr_django/product/views.py
+++ blindr_django/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .serializers import ProductSerializer
+from .models import Product
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        product = Product.objects.all()[0:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
