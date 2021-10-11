1eeac9f57a65bf00c9e83767cc430a070b63e2b5
Eric Cheng <68196762+import-brain@users.noreply.github.com>
2021-10-11 16:39:40 -0400

    feat: create website frontend, import axios

--- blindr_vue/src/views/Home.vue
+++ blindr_vue/src/views/Home.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Blindr
+        </p>
+        <p class="subtitle">
+          Window blinds as a service
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <div class="column is-3" v-for="product in latestProducts" v-bind:key="product.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail">
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-gray">${{ product.price }}</p>
+
+          View details
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Home',
   components: {
