1052f9926e8b3322fce2f563753ea0071e2ff371
Eric Cheng <68196762+import-brain@users.noreply.github.com>
2021-10-16 10:15:59 -0400

    added 'Add' functionality to vuex store

--- blindr_vue/src/store/index.js
+++ blindr_vue/src/store/index.js
@@ -10,6 +10,20 @@ export default createStore({
     isLoading: false
   },
   mutations: {
+    initializeStore(state) {
+      localStorage.getItem('cart')
+      ? state.cart = JSON.parse(localStorage.getItem('cart')) 
+      : localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      exists.length
+      ? exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      : state.cart.items.push(item)
+
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
